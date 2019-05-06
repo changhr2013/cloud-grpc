41423e9281de8ec034f495d5ebe8ead9684588ed
常浩然 <changhr@sense.com.cn>
2019-05-06 16:25:12 +0800

    [mod]为类属性添加顺序注解

--- cloud-cipher-virtual/src/main/java/com/changhr/cloud/grpc/cipher/virtual/pack/communication/TestRequest.java
+++ cloud-cipher-virtual/src/main/java/com/changhr/cloud/grpc/cipher/virtual/pack/communication/TestRequest.java
@@ -3,6 +3,7 @@ package com.changhr.cloud.grpc.cipher.virtual.pack.communication;
 import com.changhr.cloud.grpc.cipher.virtual.pack.annotation.ColumnProperty;
 import com.changhr.cloud.grpc.cipher.virtual.pack.annotation.ColumnType;
 import com.changhr.cloud.grpc.cipher.virtual.pack.AbstractPack;
+import com.changhr.cloud.grpc.cipher.virtual.pack.annotation.FieldOrder;
 import com.changhr.cloud.grpc.cipher.virtual.pack.annotation.PackType;
 import lombok.Data;
 
@@ -20,60 +21,70 @@ public class TestRequest extends AbstractPack {
     /**
      * 在进行tcp通讯时，根据此序列来对应请求包和返回包
      */
+    @FieldOrder(1)
     @ColumnProperty(type = ColumnType.LONG)
     private long seq;
 
     /**
      * 用户名
      */
+    @FieldOrder(2)
     @ColumnProperty(type = ColumnType.STRING)
     private String userName;
 
     /**
      * 密码
      */
+    @FieldOrder(3)
     @ColumnProperty(type = ColumnType.STRING)
     private String pwd;
 
     /**
      * 真实姓名
      */
+    @FieldOrder(4)
     @ColumnProperty(type = ColumnType.STRING)
     private String trueName;
 
     /**
      * 手机号
      */
+    @FieldOrder(5)
     @ColumnProperty(type = ColumnType.LONG)
     private long telNo;
 
     /**
      * 性别，0为女性、1为男性
      */
+    @FieldOrder(6)
     @ColumnProperty(type = ColumnType.BYTE)
     private byte gender;
 
     /**
      * 年龄
      */
+    @FieldOrder(7)
     @ColumnProperty(type = ColumnType.SHORT)
     private short age;
 
     /**
      * 身高
      */
+    @FieldOrder(8)
     @ColumnProperty(type = ColumnType.SHORT)
     private short height;
 
     /**
      * 体重
      */
+    @FieldOrder(9)
     @ColumnProperty(type = ColumnType.FLOAT)
     private float weight;
 
     /**
      * 教育背景
      */
+    @FieldOrder(10)
     @ColumnProperty(type = ColumnType.LIST_OBJECT)
     private List<EducationalBackground> eduBackground;
 }
