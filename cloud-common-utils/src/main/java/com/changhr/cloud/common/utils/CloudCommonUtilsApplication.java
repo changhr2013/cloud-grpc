01d699a77e93141444d8e62b310dffd4154d2fb1
常浩然 <changhr@sense.com.cn>
2019-05-31 18:49:06 +0800

    [add]初始化公共工具模块

---
+++ cloud-common-utils/src/main/java/com/changhr/cloud/common/utils/CloudCommonUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.changhr.cloud.common.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCommonUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudCommonUtilsApplication.class, args);
+    }
+
+}
